d147337605fce4cd0af8513c41d0cf371a7e97cf
Håvard Olai Kopperstad <52386166+haavardok@users.noreply.github.com>
2022-10-07 11:23:02 +0200

    Added Lab 2 First lab day

---
+++ TTK4115 - Linear Systems - Helicopter Lab 2/Plotting_template.m
@@ -0,0 +1,43 @@
+load('Lab_2_case_files\Lab2_case_1.4.3.mat')
+
+figure(1)
+
+subplot(2,1,1)
+plot(data(1,:), data(4,:), data(1,:), data(5,:), data(1,:), data(8,:))
+grid on;
+legend('Pitch [rad]', 'Pitch rate [rad/s]', 'Ref. pitch [rad]')
+xlabel('Time [s]')
+ylabel('Value')
+xlim([0 70])
+title('PITCH')
+
+subplot(2,1,2)
+plot(data(1,:), data(6,:), data(1,:), data(7,:), data(1,:), data(9,:))
+grid on;
+legend('Elevation [rad]', 'Elevation rate [rad/s]', 'Ref. elev. rate [rad/s]')
+xlabel('Time [s]')
+ylabel('Value')
+xlim([0 70])
+title('ELEVATION')
+
+load('Lab_2_case_files\Lab2_case_1.4.4.mat')
+
+figure(2)
+
+subplot(2,1,1)
+plot(data(1,:), data(4,:), data(1,:), data(5,:), data(1,:), data(8,:))
+grid on;
+legend('Pitch [rad]', 'Pitch rate [rad/s]', 'Ref. pitch [rad]')
+xlabel('Time [s]')
+ylabel('Value')
+xlim([0 70])
+title('PITCH')
+
+subplot(2,1,2)
+plot(data(1,:), data(6,:), data(1,:), data(7,:), data(1,:), data(9,:))
+grid on;
+legend('Elevation [rad]', 'Elevation rate [rad/s]', 'Ref. elev. rate [rad/s]')
+xlabel('Time [s]')
+ylabel('Value')
+xlim([0 70])
+title('ELEVATION')
